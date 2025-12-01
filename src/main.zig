bc389f2c45936b1adf6aa090a8a6416d2a7ba2cd
Jacob Enders <jacobenders1@gmail.com>
2025-12-01 17:17:17 -0500

    fix error handling

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,9 @@ const std = @import("std");
 const day1 = @import("day1");
 
 pub fn main() !void {
-    const answer_day1_part1 = day1.find_password(50, "./src/day1/input.txt");
-    std.debug.print("day1 part1: {any}\n", .{answer_day1_part1});
+    const answer_day1_part1 = try day1.find_password(50, "./src/day1/input.txt");
+    std.debug.print("day1 part1: {d}\n", .{answer_day1_part1});
 
-    const answer_day1_part2 = day1.find_password_434C49434B(50, "./src/day1/input.txt");
-    std.debug.print("day1 part2: {any}\n", .{answer_day1_part2});
+    const answer_day1_part2 = try day1.find_password_434C49434B(50, "./src/day1/input.txt");
+    std.debug.print("day1 part2: {d}\n", .{answer_day1_part2});
 }
